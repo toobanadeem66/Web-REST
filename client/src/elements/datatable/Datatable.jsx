063e8f28371c6f8058640cee0ee7d5b2d884581d
Tooba <toobakhan931@hotmail.com>
2022-05-23 23:40:08 +0500

    Manage Producta

--- client/src/elements/datatable/Datatable.jsx
+++ client/src/elements/datatable/Datatable.jsx
@@ -14,30 +14,12 @@ const columns = [
     { field: 'action', headerName: 'Action', width: 130 },
   ];
   
-//   const rows = [
-//     { id: 1, lastName: 'Snow', firstName: 'Jon', age: 35 },
-//     { id: 2, lastName: 'Lannister', firstName: 'Cersei', age: 42 },
-//     { id: 3, lastName: 'Lannister', firstName: 'Jaime', age: 45 },
-//     { id: 4, lastName: 'Stark', firstName: 'Arya', age: 16 },
-//     { id: 5, lastName: 'Targaryen', firstName: 'Daenerys', age: null },
-//     { id: 6, lastName: 'Melisandre', firstName: null, age: 150 },
-//     { id: 7, lastName: 'Clifford', firstName: 'Ferrara', age: 44 },
-//     { id: 8, lastName: 'Frances', firstName: 'Rossini', age: 36 },
-//     { id: 9, lastName: 'Roxie', firstName: 'Harvey', age: 65 },
-//   ];
+
   
 
 
 const Datatable = () => {
-    // var [prior, setPrior] = useState([
-    //   {  _id: "",
-    //   Item_Name: "",
-    //   Cat_id: "",
-    //   Item_price: Number,
-    //   Item_picture: "",
-    //   Item_desc: "",
-    //   Cat_Name: "" }
-    // ]);
+
 
   
 
@@ -88,7 +70,6 @@ const Datatable = () => {
 
 
 
-          // console.log(prior)
      
 
     
